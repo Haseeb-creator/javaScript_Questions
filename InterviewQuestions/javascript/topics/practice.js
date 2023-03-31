af063b98846b3127435f01697e3a165dceed69cc
Haseeb-creator <mdhaseeb769@gmail.com>
2023-03-31 16:07:50 +0530

    practice code file

---
+++ InterviewQuestions/javascript/topics/practice.js
@@ -0,0 +1,37 @@
+/* 1 : finding next Odd */
+
+/* input  : -> let odd = [5, 7, 9, 11, 15, 17] */
+/* output : -> 13 */
+
+/* let odd = [5, 7, 9, 11, 15, 17]
+
+for (let i = 0; i < odd.length; i++) {
+	const current = odd[i]
+	const next = current + 2
+	if (odd[i + 1] !== next) {
+		console.log(next);
+	}
+} */
+
+
+/*  2: reversing a string */
+
+let string = 'This is a Javascript Code'
+
+
+
+let split = string.split(' ')
+
+let result = ' '
+for (let i = 0; i < split.length; i++) {
+	const element = split[i];
+	let elemenentLen = [...element.split('')]
+	result += elemenentLen.reverse().join('')
+
+}
+
+
+console.log(result);
+
+
+
