242500cbb5926ac0e082dc1830aac3a4b0376a5c
Haseeb-creator <mdhaseeb769@gmail.com>
2023-03-31 16:07:19 +0530

    find next odd number

---
+++ solvingProblems.js/findNextOddNumber.js
@@ -0,0 +1,15 @@
+/* 1 : finding next Odd */
+
+/* input  : -> let odd = [5, 7, 9, 11, 15, 17] */
+/* output : -> 13 */
+
+let odd = [5, 7, 9, 11, 15, 17]
+
+
+for (let i = 0; i < odd.length; i++) {
+	const current = odd[i]
+	const next = current + 2
+	if (odd[i + 1] !== next) {
+		console.log(next);
+	}
+}
