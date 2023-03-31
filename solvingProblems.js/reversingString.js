21efc0e67368d3b5e3551200fcdab44dddeeecbf
Haseeb-creator <mdhaseeb769@gmail.com>
2023-03-31 16:07:01 +0530

    reverse a string sentence

---
+++ solvingProblems.js/reversingString.js
@@ -0,0 +1,26 @@
+/* method 1 : string  with loop */
+
+/* const reverseString = str => {
+
+	let result = ''
+	for (let i = str.length - 1; i >= 0; i--) {
+		result += str[i]
+
+	}
+	return result
+}
+
+const string = "hello world"
+console.log(reverseString(string)); */
+
+
+/* method 2 :    */
+
+
+
+/* const string = 'This is a javaScript Code'
+
+const result = string.split(' ').map((word) => word.split('').reverse().join('')
+).join(' ')
+
+console.log(result); */
